6c80c591c11021908aced5403c11da4f01b792ef
before9088-oss <before9088@gmail.com>
2026-03-07 20:40:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -76,16 +76,15 @@ async def tirar_spin(ctx, categoria, items_dict, color=discord.Color.blue()):
     inventario[user_id]["rr"][categoria] -= 1
 
     if categoria == "Clase Social":
-        ponderados = ["No tienes"] * 6 + ["Clase Plebeyo", "Clase Media", "Clase Noble"]
-        elegido = random.choice(ponderados)
-   else:
+    ponderados = ["No tienes"] * 6 + ["Clase Plebeyo", "Clase Media", "Clase Noble"]
+    elegido = random.choice(ponderados)
+    imagen = items_dict.get(elegido)
+
+    else:
     nombres = list(items_dict.keys())
     pesos = [items_dict[n]["prob"] for n in nombres]
     elegido = random.choices(nombres, weights=pesos)[0]
-
     imagen = items_dict[elegido]["img"]
-
-    imagen = items_dict.get(elegido)
 
     inventario[user_id]["objetos"][categoria] = [elegido]
     guardar_inventario()
@@ -348,6 +347,7 @@ import os
 
 TOKEN = os.getenv("TOKEN")
 bot.run(TOKEN)
+
 
 
 
